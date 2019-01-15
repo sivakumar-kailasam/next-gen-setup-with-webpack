73e452fe7351b717aae206e80aeeab8bb6b2d899
Sivakumar Kailasam <siva@rubberduck.engineering>
2019-01-15 21:09:27 +0530

    final changes

--- src/shopify/index.js
+++ src/shopify/index.js
@@ -1,1 +1,18 @@
-console.log('hi there from shopify index')
+import { AppProvider, Page, Card, Button } from '@shopify/polaris'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+let rootElement = document.createElement('div')
+rootElement.setAttribute('id', 'app')
+document.body.appendChild(rootElement)
+
+ReactDOM.render(
+  <AppProvider>
+    <Page title="Shopify page">
+      <Card sectioned>
+        <Button onClick={() => alert('Button clicked!')}>Example button</Button>
+      </Card>
+    </Page>
+  </AppProvider>,
+  rootElement
+)
