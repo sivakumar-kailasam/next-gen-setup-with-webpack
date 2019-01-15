78b6cd54500e7efd1e4644fec05684380bcc9450
Sivakumar Kailasam <siva@rubberduck.engineering>
2019-01-15 20:52:29 +0530

    Use html templates

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,16 +8,24 @@ module.exports = merge(commonConfig, {
   devServer: {
     contentBase: './dist'
   },
+  entry: {
+    shopifyDevScript: './src/shopify/index.js',
+    bigCommerceDevScript: './src/big-commerce/index.js'
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Shopify testing page',
+      template: './src/shopify/index.html',
       filename: 'shopify.html',
-      chunks: ['shopify']
+      chunks: ['shopifyDevScript', 'shopify'],
+      chunksSortMode: 'manual'
     }),
     new HtmlWebpackPlugin({
       title: 'BigCommerce testing page',
+      template: './src/big-commerce/index.html',
       filename: 'big-commerce.html',
-      chunks: ['bigCommerce']
+      chunks: ['bigCommerceDevScript', 'bigCommerce'],
+      chunksSortMode: 'manual'
     })
   ]
 })
